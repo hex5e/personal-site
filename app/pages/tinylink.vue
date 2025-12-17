3d5bffd0a8f08d01033cc5732dd7ded5f1a546a0
hex5e <hex5e@outlook.com>
2025-12-16 19:22:02 -0500

    added tinylink page

---
+++ app/pages/tinylink.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts" setup>
+    await navigateTo("https://drive.google.com/file/d/16EgPL92uCZ1jrFpWiZZBuJ2OlD5QRPCw/view?usp=sharing", {external:true});
+</script>
