8f0adfa75a55f6a59cbf38e4c284383eeb90075a
hex5e <hex5e@outlook.com>
2026-01-17 18:26:46 -0500

    update tiny-link redirect

---
+++ app/pages/tiny-link.vue
@@ -0,0 +1,7 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts" setup>
+    await navigateTo("https://drive.google.com/file/d/19kdftFBzebj98J6NSAd-j2g1hcIrx_d6/view?usp=sharing", { external: true });
+</script>
